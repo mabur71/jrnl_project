4c85a99f1da00491dc270c6c8980561a55147588
mabur71 <mabur71@yandex.ru>
2016-03-30 13:35:22 -1000

    add list station on page

--- jrnl/blog/views.py
+++ jrnl/blog/views.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 from django.shortcuts import render
 from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.core.urlresolvers import reverse
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
 from django.core.paginator import Paginator
-from .models import Records
+from .models import Records, Sites
 
 import logging
 logger = logging.getLogger(__name__)
@@ -22,6 +23,7 @@ def all_logs(request, site):
 	site_set = ['dms797', 'dms726', 'dms774']
 	if site not in site_set:
 		raise Http404("Page not found")
+	sites = Sites.objects.filter()
 	records = Records.objects.filter(site__name = site)
 	limit = 10
 	page = request.GET.get('page', 1)
@@ -29,7 +31,8 @@ def all_logs(request, site):
 	paginator.baseurl = '/' + site + '/?page='
 	page = paginator.page(page)
 	return render(request, 'all_logs.html', {
-		'site': site,
+		'sites': sites,
+		'cur_site': site,
 		'records': page.object_list,
 		'paginator': paginator,
 		'page': page,
