24cd05551aed6f7272dd564ae10918865aed717d
mabur71 <mabur71@yandex.ru>
2016-03-29 14:14:25 -1000

    models: add tables 'records', 'sites'

--- jrnl/blog/models.py
+++ jrnl/blog/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+
+
+class Site(models.Model):
+	name = models.CharField(max_length=20)
+	class Meta:
+		db_table = 'sites'
+
+
+class Records(models.Model):
+	text = models.TextField()
+	person = models.CharField(max_length=20)
+	date = models.DateTimeField(auto_now_add=True)
+	site = models.ForeignKey(Site)
+	class Meta:
+		db_table = 'records'
+		ordering = ['-id']
+
--- other/once.sh
+++ other/once.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 ./create_db.sh
-/home/operators/journal-project/jrnl/manage.py migrate 
+/home/operators/journal-project/jrnl/manage.py makemigrations
+/home/operators/journal-project/jrnl/manage.py migrate
+ 
