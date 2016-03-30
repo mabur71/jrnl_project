c2abe90e3c26422d825704f5c148070fef4d6034
mabur71 <mabur71@yandex.ru>
2016-03-30 10:11:56 -1000

    rewrite model sites: columns - name, printout.

--- jrnl/blog/models.py
+++ jrnl/blog/models.py
@@ -7,6 +7,7 @@ from django.contrib.auth.models import User
 
 class Sites(models.Model):
 	name = models.CharField(max_length=20)
+	printout = models.CharField(max_length=20)
 	class Meta:
 		db_table = 'sites'
 
--- jrnl/blog/sql/sites.sql
+++ jrnl/blog/sql/sites.sql
@@ -1,3 +1,3 @@
-INSERT INTO sites (name) VALUES ('АТС-797');
-INSERT INTO sites (name) VALUES ('АТС-774');
-INSERT INTO sites (name) VALUES ('АТС-726');
+INSERT INTO sites (name, printout) VALUES ('dms797', 'АТС-797');
+INSERT INTO sites (name, printout) VALUES ('dms774', 'АТС-774');
+INSERT INTO sites (name, printout) VALUES ('dms726', 'АТС-726');
