8167aad27d005119642e64cc6527a2991225c080
mabur71 <mabur71@yandex.ru>
2016-04-07 17:56:10 +0500

    set pretty object list in admin site

--- jrnl/blog/admin.py
+++ jrnl/blog/admin.py
@@ -5,12 +5,20 @@ from blog.models import Sites
 from blog.models import Records
 
 class SitesAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('printout',) 
+
 admin.site.register(Sites, SitesAdmin)
 #admin.site.register(Sites)
 
 
 
+class RecordsAdmin(admin.ModelAdmin):
+    def printout_site(self, obj):
+        return obj.site.printout
+    list_display = ('date', 'printout_site', 'text','person',)
+    #list_editable = ('text', 'person')
+    #list_select_related = ('site',)
+    #list_select_related = True 
 
-
-admin.site.register(Records)
+admin.site.register(Records, RecordsAdmin)
+#admin.site.register(Records)
