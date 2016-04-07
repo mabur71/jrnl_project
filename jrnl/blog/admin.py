1510c93f78bee2a62d5856a98add6824fd506dd6
mabur71 <mabur71@yandex.ru>
2016-04-07 15:50:31 +0500

    Add models Sites, Records to admin site.

--- jrnl/blog/admin.py
+++ jrnl/blog/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
+from blog.models import Sites
+from blog.models import Records
+
+class SitesAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Sites, SitesAdmin)
+#admin.site.register(Sites)
+
+
+
+
+
+admin.site.register(Records)
