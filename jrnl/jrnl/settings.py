0762f379a07e3cf05be05a375e5b1c427ce2a928
mabur71 <mabur71@yandex.ru>
2016-04-07 15:48:35 +0500

    Settings to bind styles for admin site

--- jrnl/jrnl/settings.py
+++ jrnl/jrnl/settings.py
@@ -143,4 +143,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/'
+#STATIC_ROOT = "/home/operators/journal-project/jrnl/public/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'public')
